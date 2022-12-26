9239c1bf2ea4d01c5ad0adac9556cea0b84fa49f
Donald Nkabinde <u17138770@tuks.co.za>
2022-12-26 22:13:06 +0200

    Add files via upload

---
+++ project_euler_solutions/problem_23.m
@@ -0,0 +1,65 @@
+%==========================================================================
+% A perfect number is a number for which the sum of its proper divisors is
+% exactly equal to the number. For example, the sum of the proper divisors
+% of 28 would be 1 + 2 + 4 + 7 + 14 = 28, which means that 28 is a perfect
+% number.
+%
+% A number n is called deficient if the sum of its proper divisors is less
+% than n and it is called abundant if this sum exceeds n.
+%
+% As 12 is the smallest abundant number, 1 + 2 + 3 + 4 + 6 = 16, the
+% smallest number that can be written as the sum of two abundant numbers is
+% 24. By mathematical analysis, it can be shown that all integers greater
+% than 28123 can be written as the sum of two abundant numbers. However,
+% this upper limit cannot be reduced any further by analysis even though it
+% is known that the greatest number that cannot be expressed as the sum of
+% two abundant numbers is less than this limit.
+%
+% Find the sum of all the positive integers which cannot be written as the
+% sum of two abundant numbers.
+%==========================================================================
+clear; close all;
+%--------------------------------------------------------------------------
+number = 1;
+upper_limit = 28123;
+collect_abundant_numbers = [];
+while number <= upper_limit
+    if ~isprime(number)
+        prime_factors = factor(number); % gets all prime factors
+        proper_divisors = [1,prime_factors(1)];
+        for idx = prime_factors(2:end)
+            proper_divisors = [1;idx]*proper_divisors;
+            proper_divisors = unique(proper_divisors(:)');
+            proper_divisors(find(proper_divisors == number)) = []; %#ok<FNDSB> % remove the number itself because we want values less than the number
+        end
+        proper_divisors = unique(proper_divisors);
+        if sum(proper_divisors) > number
+            collect_abundant_numbers = vertcat(collect_abundant_numbers,number);
+            fprintf('%d is an abundant number\n',number)
+        end
+    end
+    number = number + 1;
+end
+len_abundant_numbers = length(collect_abundant_numbers);
+sum_abundant_matrix = zeros(len_abundant_numbers,len_abundant_numbers);
+
+% Here we will generate a list of all sums of abundant numbers
+for idx = 1:len_abundant_numbers
+    for idy = 1:len_abundant_numbers
+        sum_abundant_matrix(idx,idy) = collect_abundant_numbers(idx) + collect_abundant_numbers(idy);
+    end
+end
+
+sum_abundant_numbers = unique(sum_abundant_matrix);
+
+total = 0;
+for ii = 1:upper_limit
+    if ~any(ii == sum_abundant_numbers)
+        total = total + ii;
+    end
+end
+%--------------------------------------------------------------------------
+fprintf('The answer is: %d\n',total)
+%--------------------------------------------------------------------------
+
+
